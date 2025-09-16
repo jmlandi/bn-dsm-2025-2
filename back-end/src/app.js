b6004098ca9bee3f85bdf6a18c1f8a833d04a68d
Landi <joaomarcospsnbr@gmail.com>
2025-09-15 22:01:42 -0300

    Aula 15/09/2025

--- back-end/src/app.js
+++ back-end/src/app.js
@@ -2,9 +2,6 @@ import express, { json, urlencoded } from 'express'
 import cookieParser from 'cookie-parser'
 import logger from 'morgan'
 
-import indexRouter from './routes/index.js'
-import usersRouter from './routes/users.js'
-
 const app = express()
 
 app.use(logger('dev'))
@@ -12,7 +9,17 @@ app.use(json())
 app.use(urlencoded({ extended: false }))
 app.use(cookieParser())
 
+/******************** ROTAS ********************/
+
+import indexRouter from './routes/index.js'
 app.use('/', indexRouter)
+
+import usersRouter from './routes/users.js'
 app.use('/users', usersRouter)
+
+import categoriasRouter from './routes/categorias.js'
+app.use('/categorias', categoriasRouter)
+
+/***********************************************/
 
 export default app
