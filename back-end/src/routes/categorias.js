2834a508bce808bc3a76a7e31072be1c04ca9c6a
Landi <joaomarcospsnbr@gmail.com>
2025-09-22 21:30:24 -0300

    Aula 22/09/2025

--- back-end/src/routes/categorias.js
+++ back-end/src/routes/categorias.js
@@ -4,5 +4,12 @@ import controller from '../controllers/categorias.js'
 const router = Router()
 
 router.post('/', controller.create)
+router.get('/', controller.retrieveAll)
+
+// :id é um PARÂMETRO DE ROTA, isto é, uma informação não fixa
+// que será enviada ao back-end na própria rota
+router.get('/:id', controller.retrieveOne)
+router.put('/:id', controller.update)
+router.delete('/:id',controller.delete)
 
 export default router
